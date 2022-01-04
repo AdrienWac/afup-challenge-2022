ba6777a137447539d94958b7d4b82b9192aa42dd
Adrien Lbt <adrienlambert725@msn.com>
2022-01-04 23:37:58 +0100

    --- Implemente method to place ship on board (To be tested)

--- battleship/GameBoard.php
+++ battleship/GameBoard.php
@@ -6,11 +6,13 @@ require "vendor/autoload.php";
 
 class GameBoard {
 
+    private array $size;
     private array $board;
     private array $ships;
 
     function __construct(array $size, mixed $value)
     {
+        $this->size = $size;
         [$maxX, $maxY] = $size;
         $this->board = array_fill(0, $maxY, array_fill(0, $maxX, $value));
     }
@@ -71,6 +73,7 @@ class GameBoard {
     }
 
 
+
     /**
      * Place one boat on game board
      *
@@ -79,24 +82,51 @@ class GameBoard {
      */
     private function placeOneShipOnBoard(\Battleship\Ship $ship) {
 
+        // Récupère la taille du board
+        [$sizeX, $sizeY] = $this->size;
+
+        // Tableau des directions
         $arrayPotentialsDirections = [\Battleship\Constants::getVerticalOrientationShip(), \Battleship\Constants::getHorizontalOrientationShip()];
 
-        $randomDirection = $arrayPotentialsDirections[rand(0, 1)];
+        // Random direction
+        $direction = $arrayPotentialsDirections[rand(0, 1)];
+
+        // On cherche un point de départ aléatoire dans le board
+        if ($direction == \Battleship\Constants::getVerticalOrientationShip()) {
+
+            $referenceSize = $sizeY;
+            $randomCoordinates = [rand(0, $sizeX - 1), rand(0, ($referenceSize - $ship->size) - 1)];
+
+        } else {
+
+            $referenceSize = $sizeX;
+            $randomCoordinates = [rand(0, ($referenceSize - $ship->size) - 1), rand(0, $sizeY - 1)];
+
+        }
+
+        // On test si tout le bateau passe
+        // Cell est dispo si toutes les cases de la taille du bateau sont libre et dans le board 
+        for ($i=0; $i < $ship->size; $i++) {
+
+            // On calcul les nouvelles coordonnées en fonction de la direction
+            $currentCoordinates = $direction == \Battleship\Constants::getVerticalOrientationShip() ? [$randomCoordinates[0], $randomCoordinates[1] + $i]  : [$randomCoordinates[0] + $i, $randomCoordinates[1]]; 
+            
+            // Si la cellule courante n'est pas dispo, on recommence à zéro
+            if (!$this->isAvailableCell($currentCoordinates)) {
+                $this->placeOneShipOnBoard($ship);
+            }
+
+            // Si la cellule n'est pas dans le board, on recommence à zéro
+            if (!$this->isInTheBoard($currentCoordinates)) {
+                $this->placeOneShipOnBoard($ship);
+            }
+
+            // Sinon on place le bateau
+            $this->setBoard($currentCoordinates, $ship->name);
+
+        }
 
 
-
-
-    }
-
-    /**
-     * Get random cell in board whos is available
-     *
-     * @param array $arrayCellCoordinatesAlreadyUsed
-     * @param string $direction
-     * @return void
-     */
-    private function getRandomAvailableCell(array $arrayCellCoordinatesAlreadyUsed, string $direction) {
-        
     }
 
     /**
@@ -106,8 +136,21 @@ class GameBoard {
      * @return boolean Is available or not
      */
     public function isAvailableCell(array $coordinates) {
+
         [$x, $y] = $coordinates;
+
         return !in_array($this->board[$y][$x], array_keys($this->ships));
+
+    }
+
+    /**
+     * Test if coordinates is in the board
+     *
+     * @param array $coordinates
+     * @return boolean
+     */
+    private function isInTheBoard(array $coordinates) {
+        return true;
     }
 
 }
